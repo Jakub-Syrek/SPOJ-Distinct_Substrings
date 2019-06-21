559f174e6a2892fb3141bd75bddb0de6f63b1aa8
Jakub-Syrek <jakubvonsyrek@gmail.com>
2019-06-21 20:44:36 +0200

    1

---
+++ Distinct Substrings/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Distinct_Substrings
+{
+    class Program
+    {
+        /*
+         Given a string, we need to find the total number of its distinct substrings.
+
+        Input
+        T- number of test cases. T<=20;
+        Each test case consists of one string, whose length is <= 1000
+
+        Output
+        For each test case output one number saying the number of distinct substrings.
+
+        Example
+        Sample Input:
+        2
+        CCCCC
+        ABABA
+
+        Sample Output:
+        5
+        9
+         */
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
